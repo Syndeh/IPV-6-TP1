9957fa266275256a8298a142c3def41780ba2dac
Syndeh <syndeh@Syndeh-Vaio.(none)>
2014-04-05 10:30:50 -0300

    se agrega la clase pelada

---
+++ src/main/java/org/uqbar/arkanoid/game/Arkanoid.java
@@ -0,0 +1,33 @@
+package org.uqbar.arkanoid.game;
+
+import java.awt.Dimension;
+
+import com.uqbar.vainilla.Game;
+
+public class Arkanoid extends Game {
+
+	@Override
+	protected void initializeResources() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void setUpScenes() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Dimension getDisplaySize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
