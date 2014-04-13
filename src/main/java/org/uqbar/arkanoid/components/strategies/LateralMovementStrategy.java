10f22e7d4d9f930cc802ba9f2c2449ac8511af33
nico <nico@ubuntu.(none)>
2014-04-12 21:01:32 -0300

    Se agregó la clase LateralMovementStrategy, que implementará el movimiento izq-der

---
+++ src/main/java/org/uqbar/arkanoid/components/strategies/LateralMovementStrategy.java
@@ -0,0 +1,14 @@
+package org.uqbar.arkanoid.components.strategies;
+
+import org.uqbar.arkanoid.components.Block;
+
+public class LateralMovementStrategy extends MovementStrategy<Block> {
+
+	@Override
+	public void move(Block gameComponent, double xDirection, double yDirection,
+			double advance) {
+
+		
+	}
+
+}
--- src/main/java/org/uqbar/arkanoid/components/strategies/MovementStrategy.java
+++ src/main/java/org/uqbar/arkanoid/components/strategies/MovementStrategy.java
@@ -5,6 +5,5 @@ import com.uqbar.vainilla.GameComponent;
 public abstract class MovementStrategy<GameComponentType extends GameComponent<?>> {
 
 	public abstract void move(GameComponentType gameComponent, double xDirection, double yDirection, double advance);
-	
-	
+
 }
